f429834cdedf6904c5735b289cb58d3bcaf14a1e
AixAria <mailboxofaria@gmail.com>
2026-09-22 18:33:22 +0330

    feat: implement hybrid execution core (apps/control-plane/build.rs)

---
+++ apps/control-plane/build.rs
@@ -0,0 +1,6 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    std::env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path()?);
+    tonic_build::configure().compile_protos(&["../../proto/execution.proto"], &["../../proto"])?;
+    println!("cargo:rerun-if-changed=../../proto/execution.proto");
+    Ok(())
+}
